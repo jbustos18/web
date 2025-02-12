37b60a4d080b2fcf68812d7560a2390dd5d70a49
jbustos18 <158625464+jbustos18@users.noreply.github.com>
2025-02-11 18:06:33 -0600

    Delete sct5.js

--- sct5.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const confirmButton = document.querySelector(".confirm-button");
-
-    confirmButton.addEventListener("click", function () {
-        alert("🎉 ¡Asistencia confirmada! Nos vemos el 15 de Febrero ❤️");
-    });
-});
